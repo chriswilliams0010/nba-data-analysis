07087465728c55310da46040656bd997ec892af0
Chris Williams <chris.williams0010@gmail.com>
2023-12-02 16:17:04 -0500

    add logging

--- stat_scraper.py
+++ stat_scraper.py
@@ -1,26 +1,47 @@
 import requests
 import time
+import logging
 
 seasons = list(range(1950, 2024))
-months = ["november", "december", "january", "february", "march", "april", "may", "june"]
+months = [
+    "november",
+    "december",
+    "january",
+    "february",
+    "march",
+    "april",
+    "may",
+    "june",
+]
 
 base_url = "https://www.basketball-reference.com/"
 
 # loop through seasons
 for season in seasons:
+    data = False
     url = base_url.format(season)
-    data = requests.get(url)
-
-    with open(f"./data/leagues/NBA_{season}.html", "w+") as f:
-        f.write(data.text)
-
+    try:
+        data = requests.get(url)
+        logging.warning(f"html successful for {url}")
+    except Exception as e:
+        logging.warning(f"An exception occured when accessing {url}: {e}")
+    if data:
+        with open(f"./data/leagues/NBA_{season}.html", "w+") as f:
+            f.write(data.text)
+            logging.warning(f"./data/leagues/NBA_{season}.html written")
     time.sleep(5)
+
 
 for season in seasons:
     for month in months:
+        data_players = False
         url = base_url + f"leagues/NBA_{season}_games-{month}.html"
-        data_players = requests.get(url)
-
-        with open(f"./data/players/{season}.html", "w+") as f:
-            f.write(data_players.text)
+        try:
+            data_players = requests.get(url)
+            logging.warning(f"html successful for {url}")
+        except:
+            logging.warning(f"An exception occured when accessing {url}: {e}")
+        if data_players:
+            with open(f"./data/players/{season}.html", "w+") as f:
+                f.write(data_players.text)
         time.sleep(5)
