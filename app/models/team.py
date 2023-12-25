a6fccc349fd74abde5c8362c6022bffa66ab850c
Chris Williams <chris.williams0010@gmail.com>
2023-12-25 11:18:43 -0500

    added Team model

---
+++ app/models/team.py
@@ -0,0 +1,22 @@
+import logging
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+logger = logging.getLogger(__name__)
+Base = declarative_base()
+
+class Team(Base):
+    """
+    Represents a team in the NBA.
+
+    Attributes:
+        team_id (int): The unique identifier for the team.
+        team_name (str): The name of the team.
+    """
+    __tablename__ = 'teams'
+
+    team_id = Column(Integer, primary_key=True)
+    team_name = Column(String(255), nullable=False)
+
+    def __repr__(self):
+        return f"<Team(team_id={self.team_id}, team_name='{self.team_name}')"
